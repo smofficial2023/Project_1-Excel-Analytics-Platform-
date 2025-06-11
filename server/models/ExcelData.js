de4d059d1bbf2abbd1eca3ab3320902563e73fd2
smofficial2023 <subhajitrocks717@gmail.com>
2025-06-11 17:47:17 +0530

    Delete server directory

--- server/models/ExcelData.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-const DataSchema = new mongoose.Schema({}, { strict: false });
-
-const ExcelData = mongoose.model('ExcelData', DataSchema);
-
-export default ExcelData;
